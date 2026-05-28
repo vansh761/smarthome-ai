feec8078845c741473d2ff67fdf2ceb5b85f5c36
Vansh Gupta <vanshgupta151006@gmail.com>
2026-05-28 20:12:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from app.routers import weather
 from app.routers import evaluation
 from app.routers import mqtt
 from app.routers import demo
+from app.routers import device_timer
 
 app = FastAPI(
     title        = "AI Smart Home Intelligence System",
@@ -83,6 +84,7 @@ app.include_router(weather.router)
 app.include_router(evaluation.router)
 app.include_router(mqtt.router)
 app.include_router(demo.router)
+app.include_router(device_timer.router)
 
 @app.get("/")
 def root():
